e4509014260d6aca68e93f9d32d7d7de725f59b5
Adarsh J <root@hackmaster.in>
2013-09-13 20:37:15 +0530

    WIP: Implemntation of initializer + control

---
+++ src/cs632/DijkstraScholten.java
@@ -0,0 +1,96 @@
+package cs632;
+
+import peersim.vector.SingleValueHolder;
+import peersim.config.*;
+import peersim.core.*;
+import peersim.transport.Transport;
+import peersim.cdsim.CDProtocol;
+import peersim.edsim.EDProtocol;
+
+public class DijkstraScholten implements CDProtocol, EDProtocol {
+
+//--------------------------------------------------------------------------
+// Initialization
+//--------------------------------------------------------------------------
+
+/**
+ * @param prefix string prefix for config properties
+ */
+public DijkstraScholten(String prefix) { super(prefix); }
+
+public long parentID;
+public boolean isActivated;
+
+//--------------------------------------------------------------------------
+// methods
+//--------------------------------------------------------------------------
+
+/**
+ * This is the standard method the define periodic activity.
+ * The frequency of execution of this method is defined by a
+ * {@link peersim.edsim.CDScheduler} component in the configuration.
+ */
+public void nextCycle( Node node, int pid )
+{
+	Linkable linkable = 
+		(Linkable) node.getProtocol( FastConfig.getLinkable(pid) );
+	if (linkable.degree() > 0)
+	{
+		Node peern = linkable.getNeighbor(
+				CommonState.r.nextInt(linkable.degree()));
+		
+		// XXX quick and dirty handling of failures
+		// (message would be lost anyway, we save time)
+		if(!peern.isUp()) return;
+		
+		((Transport)node.getProtocol(FastConfig.getTransport(pid))).
+			send(
+				node,
+				peern,
+				new AverageMessage(value,node),
+				pid);
+	}
+}
+
+//--------------------------------------------------------------------------
+
+/**
+* This is the standard method to define to process incoming messages.
+*/
+public void processEvent( Node node, int pid, Object event ) {
+		
+	AverageMessage aem = (AverageMessage)event;
+	
+	if( aem.sender!=null )
+		((Transport)node.getProtocol(FastConfig.getTransport(pid))).
+			send(
+				node,
+				aem.sender,
+				new AverageMessage(value,null),
+				pid);
+				
+	value = (value + aem.value) / 2;
+}
+
+}
+
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+/**
+* The type of a message. It contains a value of type double and the
+* sender node of type {@link peersim.core.Node}.
+*/
+class AverageMessage {
+
+	final double value;
+	/** If not null,
+	this has to be answered, otherwise this is the answer. */
+	final Node sender;
+	public AverageMessage( double value, Node sender )
+	{
+		this.value = value;
+		this.sender = sender;
+	}
+}
+
---
+++ src/cs632/DijkstraScholtenInitializer.java
@@ -0,0 +1,70 @@
+package cs632;
+
+import cs632.DijkstraScholten;
+import peersim.config.*;
+import peersim.core.*;
+
+public class DijkstraScholtenInitializer implements Control {
+
+    // ------------------------------------------------------------------------
+    // Constants
+    // ------------------------------------------------------------------------
+
+    /**
+     * The load at the peak node.
+     * 
+     * @config
+     */
+    private static final String PAR_VALUE = "value";
+
+    /**
+     * The protocol to operate on.
+     * 
+     * @config
+     */
+    private static final String PAR_PROT = "protocol";
+
+    // ------------------------------------------------------------------------
+    // Fields
+    // ------------------------------------------------------------------------
+
+    /** Value at the peak node.
+    * Obtained from config property {@link #PAR_VALUE}. */
+    private final double value;
+
+    /** Protocol identifier; obtained from config property {@link #PAR_PROT}. */
+    private final int pid;
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+
+    /**
+     * Creates a new instance and read parameters from the config file.
+     */
+    public PeakDistributionInitializer(String prefix) {
+        value = Configuration.getDouble(prefix + "." + PAR_VALUE);
+        pid = Configuration.getPid(prefix + "." + PAR_PROT);
+    }
+
+    // ------------------------------------------------------------------------
+    // Methods
+    // ------------------------------------------------------------------------
+
+    /**
+    * Initialize an aggregation protocol using a peak distribution.
+    * That is, one node will get the peek value, the others zero.
+    * @return always false
+    */
+    public boolean execute() {
+        Node rootNode = Network.get(0);
+        ((DijkstraScholten)rootNode.getProtocol(pid)).parentID = -1;
+        Linkable linkable = rootNode.getProtocol( FastConfig.getLinkable(pid) );
+
+        Node childNode = linkable.getNeighbors();
+
+        Linkable
+
+        return false;
+    }
+}
