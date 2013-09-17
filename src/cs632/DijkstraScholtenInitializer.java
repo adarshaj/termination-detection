4205f6d1689aa683278babebc5f84c69d14009d9
Anshu <anshu.avinash35@gmail.com>
2013-09-17 22:54:36 +0530

    Added graph plotting library.

--- src/cs632/DijkstraScholtenInitializer.java
+++ src/cs632/DijkstraScholtenInitializer.java
@@ -5,6 +5,7 @@ import cs632.ActivationMessage;
 import peersim.transport.Transport;
 import peersim.config.*;
 import peersim.core.*;
+import org.ubiety.ubigraph.UbigraphClient;
 
 public class DijkstraScholtenInitializer implements Control {
 
@@ -58,6 +59,16 @@ public class DijkstraScholtenInitializer implements Control {
                             pid);
             }
         }
+        UbigraphClient graph = new UbigraphClient();
+
+        int N = 10;
+        int[] vertices = new int[N];
+
+        for (int i=0; i < N; ++i)
+          vertices[i] = graph.newVertex();
+
+        for (int i=0; i < N; ++i)
+          graph.newEdge(vertices[i], vertices[(i+1)%N]);
 
         return false;
     }
